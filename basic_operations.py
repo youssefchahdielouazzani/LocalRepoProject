0ff14d302b4a3bc9a835e6feed198b8cb38d3d6e
unknown <youssefchahdielouazzani@gmail.com>
2025-09-10 12:44:29 +0100

    Exercice basic_operations terminé

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+nombre1 = 10
+nombre2 = 5
+
+somme = nombre1 + nombre2
+difference = nombre1 - nombre2
+produit = nombre1 * nombre2
+
+print(f"L'addition de {nombre1} et {nombre2} est égale à {somme}")
+print(f"La soustraction de {nombre1} et {nombre2} est égale à {difference}")
+print(f"La multiplication de {nombre1} et {nombre2} est égale à {produit}")
