3b08308dfe88eb5decb64942cf8e3bceccc28783
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-09-20 11:51:57 +0100

    Update multiplication_table.py

--- control-flow/alx_be_pythoncontrol-flo        /multiplication_table.py
+++ control-flow/alx_be_pythoncontrol-flo        /multiplication_table.py
@@ -1,1 +1,10 @@
+# multiplication_table.py
+
+# Demander à l'utilisateur de saisir un nombre
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Générer et afficher la table de multiplication de 1 à 10
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} * {i} = {result}")
 
